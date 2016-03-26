11234af75889b530b986c3eb3117d7cba9d1b64e
How-Come <820288259@qq.com>
2016-03-26 23:56:00 +0800

    code finish

---
+++ Calculator(2)/Print.h
@@ -0,0 +1,16 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<queue>
+using namespace std;
+
+class Print
+{
+    public:
+	    void print(queue<string>QUEUE);
+};
+
+#endif
--- Calculator(2)/main.cpp
+++ Calculator(2)/main.cpp
@@ -1,17 +1,20 @@
-#include "Scan.h"
-#include "Print.h"
+#include"Scan.h"
+#include"Print.h"
 #include<iostream>
-#include <stdlib.h>
-#include <string>
-#include <queue>
-
+#include<stdlib.h>
+#include<string>
+#include<queue>
 using namespace std;
+
 int main()
 {
 	string target;
 	queue<string>QUEUE;
+
 	Scan INPUT;
+
 	Print OUTPUT;
+
 	cin >> target;
 	QUEUE = INPUT.ToStringQueue(target);
 	OUTPUT.print(QUEUE);
